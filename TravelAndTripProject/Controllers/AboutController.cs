c1eff06044d388c7e81c371a29072b37b5bf550b
Oğuzhan DEMİR <oguzhandmr98@gmail.com>
2021-09-23 17:27:49 +0300

    Commit 1

---
+++ TravelAndTripProject/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelAndTripProject.Models.Classes;
+
+namespace TravelAndTripProject.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var values = context.Hakkimizdas.ToList();
+            return View(values);
+        }
+    }
+}
