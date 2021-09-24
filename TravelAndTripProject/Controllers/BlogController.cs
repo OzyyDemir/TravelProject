c2d9b9df5995421d157f3a08c200271c34e42881
Oğuzhan DEMİR <oguzhandmr98@gmail.com>
2021-09-24 19:45:54 +0300

    Commit 2

--- TravelAndTripProject/Controllers/BlogController.cs
+++ TravelAndTripProject/Controllers/BlogController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelAndTripProject.Models.Classes;
 
 namespace TravelAndTripProject.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
+        Context context = new Context();
         public ActionResult Index()
+        {
+            var blogs = context.Blogs.ToList();
+            return View(blogs);
+        }
+
+        public ActionResult BlogDetay(int id)
         {
             return View();
         }
